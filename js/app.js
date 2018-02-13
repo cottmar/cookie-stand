b2c07e7e4b244cfa4fd3f70d1090a136ef39bec2
cara ottmar <cara.ottmar@gmail.com>
2018-02-12 23:35:45 -0600

    Pushing project as-is to turn in assigment. Need to work on spacing between lists, CSS, and index.html

--- js/app.js
+++ js/app.js
@@ -25,7 +25,123 @@ var pikePlace = {
       liEl.textContent = hoursOpen[i] + ': ' + this.amountOfCookiesPurchased[i];
       // append it to the DOM
       // parent.appendChild(child)
-      ulEl.appendChild(liEl);  
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+var seaTac = {
+  name: 'Seatac',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  randomNumCustomers: function () {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour+ 1)) + this.minCustomersPerHour;
+  },
+  amountOfCookiesPurchased: [],
+  calculatedNumberOfCookiesPurchasedPerHour: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var storedAmountofCookies = this.averageCookiesPerCustomer * this.randomNumCustomers();
+      this.amountOfCookiesPurchased.push(Math.round(storedAmountofCookies));
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('1st and Pike');
+    for (var i = 0; i < this.amountOfCookiesPurchased.length; i++) {
+      // create an element
+      var liEl = document.createElement('li');
+      // give it content
+      liEl.textContent = hoursOpen[i] + ': ' + this.amountOfCookiesPurchased[i];
+      // append it to the DOM
+      // parent.appendChild(child)
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+var seattleCenter = {
+  name: 'Seattle Center',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  randomNumCustomers: function () {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour+ 1)) + this.minCustomersPerHour;
+  },
+  amountOfCookiesPurchased: [],
+  calculatedNumberOfCookiesPurchasedPerHour: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var storedAmountofCookies = this.averageCookiesPerCustomer * this.randomNumCustomers();
+      this.amountOfCookiesPurchased.push(Math.round(storedAmountofCookies));
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('1st and Pike');
+    for (var i = 0; i < this.amountOfCookiesPurchased.length; i++) {
+      // create an element
+      var liEl = document.createElement('li');
+      // give it content
+      liEl.textContent = hoursOpen[i] + ': ' + this.amountOfCookiesPurchased[i];
+      // append it to the DOM
+      // parent.appendChild(child)
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+var capitolHill = {
+  name: 'Capitol Hill',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  randomNumCustomers: function () {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour+ 1)) + this.minCustomersPerHour;
+  },
+  amountOfCookiesPurchased: [],
+  calculatedNumberOfCookiesPurchasedPerHour: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var storedAmountofCookies = this.averageCookiesPerCustomer * this.randomNumCustomers();
+      this.amountOfCookiesPurchased.push(Math.round(storedAmountofCookies));
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('1st and Pike');
+    for (var i = 0; i < this.amountOfCookiesPurchased.length; i++) {
+      // create an element
+      var liEl = document.createElement('li');
+      // give it content
+      liEl.textContent = hoursOpen[i] + ': ' + this.amountOfCookiesPurchased[i];
+      // append it to the DOM
+      // parent.appendChild(child)
+      ulEl.appendChild(liEl);
+    }
+  }
+};
+
+var alkiBeach = {
+  name: 'Alki Beach',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  randomNumCustomers: function () {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour+ 1)) + this.minCustomersPerHour;
+  },
+  amountOfCookiesPurchased: [],
+  calculatedNumberOfCookiesPurchasedPerHour: function () {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var storedAmountofCookies = this.averageCookiesPerCustomer * this.randomNumCustomers();
+      this.amountOfCookiesPurchased.push(Math.round(storedAmountofCookies));
+    }
+  },
+  render: function() {
+    var ulEl = document.getElementById('1st and Pike');
+    for (var i = 0; i < this.amountOfCookiesPurchased.length; i++) {
+      // create an element
+      var liEl = document.createElement('li');
+      // give it content
+      liEl.textContent = hoursOpen[i] + ': ' + this.amountOfCookiesPurchased[i];
+      // append it to the DOM
+      // parent.appendChild(child)
+      ulEl.appendChild(liEl);
     }
   }
 };
@@ -33,3 +149,15 @@ var pikePlace = {
 pikePlace.randomNumCustomers();
 pikePlace.calculatedNumberOfCookiesPurchasedPerHour();
 pikePlace.render();
+seaTac.randomNumCustomers();
+seaTac.calculatedNumberOfCookiesPurchasedPerHour();
+seaTac.render();
+seattleCenter.randomNumCustomers();
+seattleCenter.calculatedNumberOfCookiesPurchasedPerHour();
+seattleCenter.render();
+capitolHill.randomNumCustomers();
+capitolHill.calculatedNumberOfCookiesPurchasedPerHour();
+capitolHill.render();
+alkiBeach.randomNumCustomers();
+alkiBeach.calculatedNumberOfCookiesPurchasedPerHour();
+alkiBeach.render();
